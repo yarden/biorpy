c7f23e7ccc90cc96b8d8bdbb5e9a60b9e1057599
Noah Spies <nspies@stanford.edu>
2013-03-30 12:23:41 -0700

    Adding errbar function for plotting

--- biorpy/plotting.py
+++ biorpy/plotting.py
@@ -6,10 +6,13 @@
 import pandas
 import rpy2
 
-from rpy2.robjects import r
+#from rpy2.robjects import r
+from biorpy.betteR import BetteR
 import rpy2.robjects.numpy2ri
 import pandas.rpy.common as com
 rpy2.robjects.numpy2ri.activate()
+
+r = BetteR()
 
 def plot(x, y, **kwargs):
     """
@@ -25,4 +28,13 @@ def plot(x, y, **kwargs):
     if "ylab" not in kwargs:
         kwargs["ylab"] = ""
     # Call r.plot here with supplied keyword arguments...
+
+def errbars(x=None, y=None, x_lower=None, x_upper=None, y_lower=None, y_upper=None, length=0.08, *args, **kwdargs):
+    if y is not None and  x_lower is not None  and x_upper is not None:
+        r.arrows(x_lower, y, x_upper, y, angle = 90, code = 3, length = length, *args, **kwdargs)
+    elif x is not None and y_lower is not None and y_upper is not None:
+        r.arrows(x, y_lower, x, y_upper, angle = 90, code = 3, length = length, *args, **kwdargs)
+    else:
+        raise Exception("must define either (y, x_lower, x_upper) or (x, y_lower, y_upper)")
+            
     
